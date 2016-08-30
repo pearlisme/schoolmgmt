f233b27dc453183d4a134e0eb83f62bb84b0a916
pearl <r.muthubharathi@live.in>
2016-08-30 18:58:45 +0000

    added mongo support

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require("http");
-http.createServer(function (req,res){
-    
-    res.writeHead(200, {'Context-Type': 'text/plain'});
-    res.end('Hello World');
-}).listen(process.env.PORT, process.env.IP);
