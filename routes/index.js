05643e7ee697dc755ad39bec4e16e6c9be556aa3
pearl <r.muthubharathi@live.in>
2016-09-03 13:11:55 +0000

    changes on cloud 9

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ebbas' });
+   res.sendFile('../html/home.html');
 });
 router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Ebbas' });
