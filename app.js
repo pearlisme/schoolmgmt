a32c3376ffe18734f25f54cd388a0543f43cef07
pearl <r.muthubharathi@live.in>
2016-09-04 14:06:33 +0000

    adding view engine as HTML

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var users = require('./routes/students');
-var users = require('./routes/staff');
-
+//var xlsjs = require('xlsjs');
+///var routes = require('./routes/index');
+//var users = require('./routes/users');
+//var students = require('./routes/students');
+var staffs = require('./routes/staffs');
+var cons = require('consolidate');
 var app = express();
 
 // Retrieve
@@ -22,9 +22,38 @@ MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
   }
 });
 
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/views/html/login.html');
+});
+app.get('/home', function(request, response) {
+    response.sendFile(__dirname + '/views/html/home.html');
+});
+app.get('/admin', function(request, response) {
+    response.sendFile(__dirname + '/public/admin.html');
+});
+app.get('/student/add', function(request, response) {
+    response.sendFile(__dirname + '/public/home.html');
+});
+app.get('/student/modify', function(request, response) {
+    response.sendFile(__dirname + '/public/home.html');
+});app.get('/admin', function(request, response) {
+    response.sendFile(__dirname + '/public/home.html');
+});
+app.get('/student/delete', function(request, response) {
+    response.sendFile(__dirname + '/public/home.html');
+});
+app.get('/student/exports', function(request, response) {
+    response.sendFile(__dirname + '/public/home.html');
+});
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+//app.engine('html', cons.swig)
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'html');
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -34,8 +63,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+//app.use('/students', students);
+//app.use('/staffs', staffs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ebbas' });
-   res.sendFile('../html/home.html');
-});
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Ebbas' });
-});
-router.get('/about', function(req, res, next) {
-  res.render('aboutus', { title: 'Ebbas ' });
+   //res.sendFile('../../views/home.html');
 });
 
-router.get('/add', function(req, res, next) {
- res.render('add_student', { title: 'Ebbas ' });
-});
 module.exports = router;
